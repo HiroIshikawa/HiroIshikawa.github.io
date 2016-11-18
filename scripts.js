f3e4165b7316f0042c3c8fc9e638c4516b7f82ce
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-17 21:14:09 -0800

    React update

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-ReactDOM.render(<h1>Hellow World</h1>, document.getElementById('root'));
+var HelloMessage = React.createClass({
+	render: function() {
+		return <h1>Hello {this.props.message}</h1>;
+	}
+});
+
+ReactDOM.render(<HelloMessage message="World" />, document.getElementById('root'));
