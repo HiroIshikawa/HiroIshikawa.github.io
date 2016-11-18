21024be607887e2cf52326c1cce41417a0c25343
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-17 21:23:01 -0800

    React extended

--- scripts.js
+++ scripts.js
@@ -1,7 +1,39 @@
-var HelloMessage = React.createClass({
+// var HelloMessage = React.createClass({
+// 	render: function() {
+// 		return <h1>Hello {this.props.message}</h1>;
+// 	}
+// });
+
+// ReactDOM.render(<HelloMessage message="World" />, document.getElementById('root'));
+
+
+var ToggleText = React.createClass({
+	getInitialState: function() {
+		return {
+			showDefault: true
+		}
+	},
+
+	toggle: function(e) {
+		e.preventDefault();
+
+		this.setState({ showDefault: |this.state.showDefault })
+	},
+
 	render: function() {
-		return <h1>Hello {this.props.message}</h1>;
+		var message = this.props.default;
+
+		if (!this.state.showDefault) {
+			message = this.props.alt;
+		}
+
+		return (
+			<div>
+				<h1>Hello {message}!</h1>
+				<a href" onClick={this.toggle}>Toggle</a>
+			</div>
+		);
 	}
 });
 
-ReactDOM.render(<HelloMessage message="World" />, document.getElementById('root'));
+ReactDOM.render(<ToggleText default="World" alt="Mars" />, document.getElementById('root'));
