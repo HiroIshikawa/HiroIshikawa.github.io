3b674de5fafe244479d1dff3ecbc6d1629cf5ae6
HiroIshikawa <hiro52ishikawa@gmail.com>
2016-11-17 21:26:39 -0800

    React correct2

--- scripts.js
+++ scripts.js
@@ -8,32 +8,37 @@
 
 
 var ToggleText = React.createClass({
-	getInitialState: function() {
-		return {
-			showDefault: true
-		}
-	},
-
-	toggle: function(e) {
-		e.preventDefault();
-
-		this.setState({ showDefault: |this.state.showDefault })
-	},
-
-	render: function() {
-		var message = this.props.default;
-
-		if (!this.state.showDefault) {
-			message = this.props.alt;
-		}
-
-		return (
-			<div>
-				<h1>Hello {message}!</h1>
-				<a href="" onClick={this.toggle}>Toggle</a>
-			</div>
-		);
-	}
+  getInitialState: function () {
+    return {
+      showDefault: true
+    }
+  },
+  
+  toggle: function (e) {
+    // Prevent following the link.
+    e.preventDefault();
+    
+    // Invert the chosen default.
+    // This will trigger an intelligent re-render of the component.
+    this.setState({ showDefault: !this.state.showDefault })
+  },
+  
+  render: function () {
+    // Default to the default message.
+    var message = this.props.default;
+    
+    // If toggled, show the alternate message.
+    if (!this.state.showDefault) {
+      message = this.props.alt;
+    }
+    
+    return (
+      <div>
+        <h1>Hello {message}!</h1>
+        <a href="" onClick={this.toggle}>Toggle</a>
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(<ToggleText default="World" alt="Mars" />, document.getElementById('root'));
